4dc117ee239d5775143d8e57c0e87a9d3f17b206
Shineily <shineily@hotmail.com>
2020-10-20 16:05:59 -0500

    doc reducer

---
+++ src/reducers/uiReducer.js
@@ -0,0 +1,33 @@
+import { types } from '../types/types';
+
+const initialState = {
+  loading: false,
+  mgsError: null,
+};
+
+export const uiReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.uiStartLoading:
+      return {
+        ...state,
+        loading: true,
+      };
+    case types.uiFinishLoading:
+      return {
+        ...state,
+        loading: false,
+      };
+    case types.uiSetError:
+      return {
+        ...state,
+        mgsError: action.payload,
+      };
+    case types.uiRemoveError:
+      return {
+        ...state,
+        mgsError: null,
+      };
+    default:
+      return state;
+  }
+};
