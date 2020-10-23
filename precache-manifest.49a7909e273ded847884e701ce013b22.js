1f169c1c6051721e6bac13fac50228e0536cae23
Shineily <shineily@hotmail.com>
2020-10-23 13:32:39 -0500

    Updates

---
+++ precache-manifest.49a7909e273ded847884e701ce013b22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e03d3f4fb65154c64ae9d726365989",
+    "url": "/Journal-App/index.html"
+  },
+  {
+    "revision": "0c4fdcfbb8f3d14d89c6",
+    "url": "/Journal-App/static/css/2.3d056dbd.chunk.css"
+  },
+  {
+    "revision": "f7bca6c99f10ebe31f47",
+    "url": "/Journal-App/static/css/main.b3018b1b.chunk.css"
+  },
+  {
+    "revision": "0c4fdcfbb8f3d14d89c6",
+    "url": "/Journal-App/static/js/2.a64e3291.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/Journal-App/static/js/2.a64e3291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bca6c99f10ebe31f47",
+    "url": "/Journal-App/static/js/main.c542dc1d.chunk.js"
+  },
+  {
+    "revision": "6c6aa7f30f8f30ffb131",
+    "url": "/Journal-App/static/js/runtime-main.2a0e8efc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Journal-app/precache-manifest.3cd8b39919c36d34f8dab7740ca246a3.js"
+  "/Journal-App/precache-manifest.49a7909e273ded847884e701ce013b22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Journal-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Journal-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
