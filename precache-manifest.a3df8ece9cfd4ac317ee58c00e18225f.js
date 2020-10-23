9cfbb18ab58b568f088575eabfda37d51e2d7f91
Shineily <shineily@hotmail.com>
2020-10-23 12:39:04 -0500

    Updates

---
+++ precache-manifest.a3df8ece9cfd4ac317ee58c00e18225f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8146163b5ea0197e246f70423b3e4ae4",
+    "url": "/journal-app/index.html"
+  },
+  {
+    "revision": "5dacc3e1887260a57b8b",
+    "url": "/journal-app/static/css/2.3d056dbd.chunk.css"
+  },
+  {
+    "revision": "f7bca6c99f10ebe31f47",
+    "url": "/journal-app/static/css/main.b3018b1b.chunk.css"
+  },
+  {
+    "revision": "5dacc3e1887260a57b8b",
+    "url": "/journal-app/static/js/2.d5174f2b.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/journal-app/static/js/2.d5174f2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bca6c99f10ebe31f47",
+    "url": "/journal-app/static/js/main.c542dc1d.chunk.js"
+  },
+  {
+    "revision": "7369199aa78be9551bdd",
+    "url": "/journal-app/static/js/runtime-main.01427f0e.js"
+  }
+]);
